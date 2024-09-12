cd7ae725cbc99f308eb7353bb485cab2fbab17c3
daniel.malzl <daniel.malzl@clip-login-0.cbe.vbc.ac.at>
2024-09-12 16:41:41 +0200

    run atacseq pipeline

---
+++ scripts/run_atacseq.sh
@@ -0,0 +1,12 @@
+module load build-env/f2022
+module load nextflow/23.10.1
+
+nextflow run nf-core/atacseq \
+	--input resource/atac_samples.csv \
+	--outdir atacseq \
+	--genome GRCh38 \
+	--aligner bwa \
+	--read_length 150 \
+	-w /scratch-cbe/users/daniel.malzl/work/ \
+	-profile cbe \
+	-resume
