068e4c569c9546c3966012935947e0596bc7bcbd
daniel.malzl <daniel.malzl@clip-login-0.cbe.vbc.ac.at>
2024-10-23 16:51:37 +0200

    fix paths and rename files

--- scripts/cutandrun/cutandrun.sh
+++ scripts/cutandrun/cutandrun.sh
@@ -2,14 +2,14 @@ module load build-env/f2022
 module load nextflow/23.04.2
 
 nextflow run nf-core/cutandrun \
-	-c ../config.conf \
+	-c resource/cnr_config.conf \
 	-profile cbe \
 	-w /scratch-cbe/users/daniel.malzl/cpm \
-	--input ../samples.csv \
+	--input resource/cnr_samples.csv \
 	--outdir results \
 	--genome GRCh38 \
 	--peakcaller seacr,macs2 \
-	--blacklist ../cutandrun_blacklist_hg38.bed \
+	--blacklist resource/cnr_blacklist_hg38.bed \
 	--normalisation_mode CPM \
 	--seacr_stringent stringent \
 	--seacr_norm norm \
